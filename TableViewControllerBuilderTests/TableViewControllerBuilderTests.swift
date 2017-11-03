c5a9acbb66d56d55246ece2444e640c6c57412fd
Corneliu Chitanu <cornel@dolfn.com>
2017-11-03 17:37:53 +0200

    When adding a single section, test that number of section is 1 and ... the header view is added to the table view.

--- TableViewControllerBuilderTests/TableViewControllerBuilderTests.swift
+++ TableViewControllerBuilderTests/TableViewControllerBuilderTests.swift
@@ -11,11 +11,14 @@ import XCTest
 
 class TableViewControllerBuilderTests: XCTestCase {
     
-    var sut: TableViewControllerBuilder<HeaderDisplayDataMock, CellDisplayDataMock>!
+    var sut: TableViewControllerBuilder<FakeHeaderDisplayData, FakeCellDisplayData>!
+    var viewModel = TableViewModelStub()
+    var navController: UINavigationController!
     
     override func setUp() {
         super.setUp()
-        let viewModel = TableViewModelMock()
+        
+        navController = UINavigationController()
         let cellConfiguratorFactory = CellConfiguratorFactoryMock()
         sut = TableViewControllerBuilder(viewModel: viewModel, cellConfiguratorFactory: cellConfiguratorFactory)
     }
@@ -33,7 +36,6 @@ class TableViewControllerBuilderTests: XCTestCase {
         _ = sut.tableViewController
         XCTAssertNotNil(sut.tableViewDelegate)
     }
-
     
     func test_IsHavingATableView() {
         XCTAssertNotNil(firstView(), "First view in Table View Controller should not be nil")
@@ -47,33 +49,73 @@ class TableViewControllerBuilderTests: XCTestCase {
         return sut.tableViewController.view.subviews.first
     }
     
+    func test_WhenAddingHeaderConfigurator_ShouldAddHeadersToTableView() {
+        let tableView = firstView() as! UITableView
+        let headerConfiguratorFactory = HeaderConfiguratorFactoryMock()
+        sut.addHeaders(with: headerConfiguratorFactory)
+        guard let numberOfSections = tableView.dataSource?.numberOfSections?(in: tableView) else {
+            XCTFail("table view does not have a data source")
+            return
+        }
+        XCTAssertEqual(numberOfSections, 1)
+    }
+    
+    func test_ExistingHeaderViewForFirstSection() {
+        let tableView = firstView() as! UITableView
+        let headerConfiguratorFactory = HeaderConfiguratorFactoryMock()
+        sut.addHeaders(with: headerConfiguratorFactory)
+        let _ = sut.tableViewDelegate
+        XCTAssertNotNil(tableView.delegate!.tableView!(tableView, viewForHeaderInSection: 0))
+    }
 }
 
 extension TableViewControllerBuilderTests {
     
-    class HeaderDisplayDataMock: HeightFlexible {
+    class FakeHeaderDisplayData: HeightFlexible {
         var height: Int = 0
     }
-    class CellDisplayDataMock: HeightFlexible {
+    class FakeCellDisplayData: HeightFlexible {
         var height: Int = 0
     }
     
-    class TableViewModelMock: TableViewModel {
+    struct TableViewModelStub: TableViewModel {
         
-        typealias HeaderDisplayDataType = HeaderDisplayDataMock
-        typealias CellDisplayDataType = CellDisplayDataMock
+        struct SectionDisplayDataStub: SectionDisplayData {
+            typealias HeaderDisplayDataType = FakeHeaderDisplayData
+            typealias CellDisplayDataType = FakeCellDisplayData
+            
+            var headerDisplayData: TableViewControllerBuilderTests.FakeHeaderDisplayData
+            var sectionRowsData: [TableViewControllerBuilderTests.FakeCellDisplayData]
+        }
         
-        var sectionsDisplayData = [AnySectionDisplayData<TableViewControllerBuilderTests.HeaderDisplayDataMock, TableViewControllerBuilderTests.CellDisplayDataMock>]()
+        typealias HeaderDisplayDataType = FakeHeaderDisplayData
+        typealias CellDisplayDataType = FakeCellDisplayData
         
         var shouldBeScrollable: Bool = false
+        var sectionsDisplayData: [AnySectionDisplayData<TableViewControllerBuilderTests.FakeHeaderDisplayData, TableViewControllerBuilderTests.FakeCellDisplayData>]
+        
+        init() {
+            let headerDisplayData = FakeHeaderDisplayData()
+            let section = SectionDisplayDataStub(headerDisplayData: headerDisplayData, sectionRowsData: [])
+            let erasedSection = AnySectionDisplayData(sectionDisplayData: section)
+            sectionsDisplayData = [erasedSection]
+        }
     }
     
     class CellConfiguratorFactoryMock: CellConfiguratorFactory {
-        typealias CellDisplayData = CellDisplayDataMock
+        typealias CellDisplayData = FakeCellDisplayData
         
-        func cellConfigurator(with cellDisplayData: TableViewControllerBuilderTests.CellDisplayDataMock) -> AnyClosureCellConfigurator<TableViewControllerBuilderTests.CellDisplayDataMock> {
-            let configurator = ClosureCellConfigurator { (cell: UITableViewCell, data: CellDisplayDataMock) in }
+        func cellConfigurator(with cellDisplayData: TableViewControllerBuilderTests.FakeCellDisplayData) -> AnyClosureCellConfigurator<TableViewControllerBuilderTests.FakeCellDisplayData> {
+            let configurator = ClosureCellConfigurator { (cell: UITableViewCell, data: FakeCellDisplayData) in }
             return AnyClosureCellConfigurator(base:configurator)
+        }
+    }
+    
+    class HeaderConfiguratorFactoryMock: HeaderConfiguratorFactory {
+        typealias HeaderDisplayDataType = FakeHeaderDisplayData
+        func configurator(with displayDataType: TableViewControllerBuilderTests.FakeHeaderDisplayData) -> AnyHeaderViewConfigurator<TableViewControllerBuilderTests.FakeHeaderDisplayData>? {
+            let configurator = ClosureHeaderConfigurator {(displayData: FakeHeaderDisplayData, UITableViewHeaderFooterView) in }
+            return AnyHeaderViewConfigurator(baseConfigurator: configurator)
         }
     }
 }
