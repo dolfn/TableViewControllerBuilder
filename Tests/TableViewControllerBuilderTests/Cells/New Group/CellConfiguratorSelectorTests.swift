d5619d1e1d09f11bfa0936f639ce989f04e053b4
Andrei Nastasiu <nastasiuandrei@gmail.com>
2018-02-13 14:45:20 +0200

    Calling configure cell creates the required configurator

--- Tests/TableViewControllerBuilderTests/Cells/New Group/CellConfiguratorSelectorTests.swift
+++ Tests/TableViewControllerBuilderTests/Cells/New Group/CellConfiguratorSelectorTests.swift
@@ -11,12 +11,33 @@ import XCTest
 
 class CellConfiguratorSelectorTests: XCTestCase {
     
+    var cellConfiguratorFactory: CellConfiguratorFactoryMock!
+    var sut: CellConfiguratorSelector<FakeCellDisplayData>!
+    var tableView: UITableView!
+    
+    override func setUp() {
+        super.setUp()
+        cellConfiguratorFactory = CellConfiguratorFactoryMock()
+        sut = CellConfiguratorSelector(configuratorFactory: cellConfiguratorFactory)
+        tableView = UITableView()
+    }
+    
+    override func tearDown() {
+        cellConfiguratorFactory = nil
+        sut = nil
+        tableView = nil
+        super.tearDown()
+    }
+    
     func test_CallingRegister_DoesNotChangeTableViewState() {
-        let cellConfiguratorFactory = CellConfiguratorFactoryMock()
-        let sut = CellConfiguratorSelector(configuratorFactory: cellConfiguratorFactory)
-        let tableView = UITableView()
         sut.register(in: tableView)
         XCTAssertNil(tableView.dequeueReusableCell(withIdentifier: "FakeCellDisplayData"))
+    }
+    
+    func test_CallingConfigureCell_CreatesNeededConfigurator() {
+        _ = sut.configuredCell(in: tableView, at: IndexPath(row: 0, section: 0), with: FakeCellDisplayData())
+        XCTAssertEqual(cellConfiguratorFactory.configurator?.reuseIdentifier, "UITableViewCell")
+        XCTAssertEqual(cellConfiguratorFactory.numberOfTimesCalledToConfigureRow, 1)
     }
     
 }
--- Tests/TableViewControllerBuilderTests/Doubles/CellConfiguratorFactoryMock.swift
+++ Tests/TableViewControllerBuilderTests/Doubles/CellConfiguratorFactoryMock.swift
@@ -7,14 +7,17 @@ import Foundation
 import UIKit
 
 class CellConfiguratorFactoryMock: CellConfiguratorFactory {
+    
     typealias CellDisplayData = FakeCellDisplayData
     
     var numberOfTimesCalledToConfigureRow = 0
+    var configurator: ClosureCellConfigurator<FakeCellDisplayData, UITableViewCell>?
     
     func cellConfigurator(with cellDisplayData: FakeCellDisplayData) -> AnyClosureCellConfigurator<FakeCellDisplayData> {
         let configurator = ClosureCellConfigurator {[weak self] (cell: UITableViewCell, data: FakeCellDisplayData) in
             self?.numberOfTimesCalledToConfigureRow += 1
         }
+        self.configurator = configurator
         return configurator.erased
     }
 }
