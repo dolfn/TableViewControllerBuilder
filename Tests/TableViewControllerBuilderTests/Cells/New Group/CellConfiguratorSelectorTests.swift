0cb058935000a53e6abb42eddedc4432c5c16836
Andrei Nastasiu <nastasiuandrei@gmail.com>
2018-02-13 14:56:47 +0200

    Calling to reconfigure cell, creates required configurator

--- Tests/TableViewControllerBuilderTests/Cells/New Group/CellConfiguratorSelectorTests.swift
+++ Tests/TableViewControllerBuilderTests/Cells/New Group/CellConfiguratorSelectorTests.swift
@@ -1,8 +1,5 @@
 //
 //  CellConfiguratorSelectorTests.swift
-//  TableViewControllerBuilder-iOS Tests
-//
-//  Created by Andrei Nastasiu on 09/02/2018.
 //  Copyright © 2018 Dolfn. All rights reserved.
 //
 
@@ -14,18 +11,21 @@ class CellConfiguratorSelectorTests: XCTestCase {
     var cellConfiguratorFactory: CellConfiguratorFactoryMock!
     var sut: CellConfiguratorSelector<FakeCellDisplayData>!
     var tableView: UITableView!
+    var indexPath: IndexPath!
     
     override func setUp() {
         super.setUp()
         cellConfiguratorFactory = CellConfiguratorFactoryMock()
         sut = CellConfiguratorSelector(configuratorFactory: cellConfiguratorFactory)
         tableView = UITableView()
+        indexPath = IndexPath(row: 0, section: 0)
     }
     
     override func tearDown() {
         cellConfiguratorFactory = nil
         sut = nil
         tableView = nil
+        indexPath = nil
         super.tearDown()
     }
     
@@ -34,10 +34,17 @@ class CellConfiguratorSelectorTests: XCTestCase {
         XCTAssertNil(tableView.dequeueReusableCell(withIdentifier: "FakeCellDisplayData"))
     }
     
-    func test_CallingConfigureCell_CreatesNeededConfigurator() {
-        _ = sut.configuredCell(in: tableView, at: IndexPath(row: 0, section: 0), with: FakeCellDisplayData())
+    func test_CallingToConfigureCell_CreatesNeededConfigurator() {
+        XCTAssertNil(cellConfiguratorFactory.configurator)
+        _ = sut.configuredCell(in: tableView, at: indexPath, with: FakeCellDisplayData())
         XCTAssertEqual(cellConfiguratorFactory.configurator?.reuseIdentifier, "UITableViewCell")
         XCTAssertEqual(cellConfiguratorFactory.numberOfTimesCalledToConfigureRow, 1)
+    }
+    
+    func test_CallingToReconfigureCell_CreatesNeededConfigurator() {
+        XCTAssertNil(cellConfiguratorFactory.configurator)
+        _ = sut.reconfigureCell(in: tableView, at: indexPath, with: FakeCellDisplayData())
+        XCTAssertEqual(cellConfiguratorFactory.configurator?.reuseIdentifier, "UITableViewCell")
     }
     
 }
