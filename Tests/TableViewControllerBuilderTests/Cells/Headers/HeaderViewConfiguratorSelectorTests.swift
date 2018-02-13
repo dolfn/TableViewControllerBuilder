fc2f39011ef90da30698b00066dd5269d9c1d637
Andrei Nastasiu <nastasiuandrei@gmail.com>
2018-02-13 16:38:21 +0200

    Configure header view with given configurator

--- Tests/TableViewControllerBuilderTests/Cells/Headers/HeaderViewConfiguratorSelectorTests.swift
+++ Tests/TableViewControllerBuilderTests/Cells/Headers/HeaderViewConfiguratorSelectorTests.swift
@@ -8,11 +8,37 @@ import XCTest
 
 class HeaderViewConfiguratorSelectorTests: XCTestCase {
     
+    var headerConfiguratorFactorySpy: HeaderConfiguratorFactorySpy!
+    var sut: HeaderViewConfiguratorSelector<FakeHeaderDisplayData>!
+    var tableView: UITableView!
+    var configurator: ClosureHeaderConfigurator<FakeHeaderDisplayData, UITableViewHeaderFooterView>!
+    override func setUp() {
+        super.setUp()
+        headerConfiguratorFactorySpy = HeaderConfiguratorFactorySpy()
+        sut = HeaderViewConfiguratorSelector(configuratorFactory: headerConfiguratorFactorySpy)
+        tableView = UITableView()
+    }
+    
+    override func tearDown() {
+        headerConfiguratorFactorySpy = nil
+        sut = nil
+        tableView = nil
+        configurator = nil
+        super.tearDown()
+    }
+    
     func test_IfHeaderConfigurationFactoryDoesNotProvideConfigurator_ReturnNil() {
-        let headerConfiguratorFactorySpy = HeaderConfiguratorFactorySpy()
-        let sut = HeaderViewConfiguratorSelector(configuratorFactory: headerConfiguratorFactorySpy)
-        let tableView = UITableView()
         XCTAssertNil(sut.configuredHeader(in: tableView, at: 0, with: FakeHeaderDisplayData()))
+    }
+    
+    func test_ConfigureHeaderView_WithGivenConfigurator() {
+        let expect = expectation(description: "Configure given tableview header footer view")
+        configurator = ClosureHeaderConfigurator<FakeHeaderDisplayData, UITableViewHeaderFooterView> { (_, _) in
+            expect.fulfill()
+        }
+        headerConfiguratorFactorySpy.configuratorToReturn = configurator.erased
+        _ = sut.configuredHeader(in: tableView, at: 0, with: FakeHeaderDisplayData())
+        wait(for: [expect], timeout: 0.5)
     }
     
 }
